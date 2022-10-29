d4bcbd895ea68ac53c7d2a42df2795c21c80d1aa
Thalia <thalia.luz.tenorio@gmail.com>
2022-10-29 15:08:55 -0300

    Entrega Final Curso React Firebase Checkout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,19 +8,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cart from './Components/Cart';
 import Checkout from './Components/Checkout'
 import ItemDetailContainer from './Components/ItemDetailContainer';
+import { CartProvider } from './contexts/CartContext';
 
 
 function App() {
     return (
       <BrowserRouter basename='/Preentrega-Tenorio-my-app'>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<ItemListContainer greeting={'Tienda Online'} />} />
-          <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Bienvenidos a mi tienda'} />} />
-          <Route path='/item/:id' element={<ItemDetailContainer />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/checkout' element={<Checkout />} />
-        </Routes>
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer greeting={'Tienda Online'} />} />
+            <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Bienvenidos a mi tienda'} />} />
+            <Route path='/item/:id' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/checkout' element={<Checkout />} />
+          </Routes>
+        </CartProvider>
       </BrowserRouter>
     );
 }
